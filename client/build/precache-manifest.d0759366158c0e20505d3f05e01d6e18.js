64b95002df779bb0ecb937439422e3e93a70c9d8
Charizard13 <67857533+Charizard13@users.noreply.github.com>
2020-11-16 17:26:45 +0200

    Add files via upload

---
+++ client/build/precache-manifest.d0759366158c0e20505d3f05e01d6e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d8089ca6537fa5c4f1975a8062ebc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "591e37cb89aff71e3bd9",
+    "url": "/static/css/main.ea99efd6.chunk.css"
+  },
+  {
+    "revision": "368a3cc8dda235b03709",
+    "url": "/static/js/2.5430470b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5430470b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591e37cb89aff71e3bd9",
+    "url": "/static/js/main.b5c2b81b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
